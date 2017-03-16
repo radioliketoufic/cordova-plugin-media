7eaad5be78f8cb7ba5d3c368a8b5420d9bfa85e0
touficzayed <toufic_zayed@yahoo.fr>
2017-03-16 12:03:06 +0100

    test merge with cordova media with compression

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -644,7 +644,7 @@
             // get the audioSession and set the category to allow recording when device is locked or ring/silent switch engaged
             if ([weakSelf hasAudioSession]) {
                 if (![weakSelf.avSession.category isEqualToString:AVAudioSessionCategoryPlayAndRecord]) {
-                    [weakSelf.avSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionMixWithOthers error:nil];
+                    [weakSelf.avSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionMixWithOthers|AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
                 }
 
                 if (![weakSelf.avSession setActive:YES error:&error]) {
